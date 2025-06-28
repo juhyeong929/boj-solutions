a0db2eae2d5385acd6a6055d136be8fa423abc8e
Jhyeong09 <kimkjo0409@naver.com>
2025-06-28 22:08:28 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+  int n;
+  int total = 0;
+
+  scanf("%d",&n);
+
+  for(int i = 1; i<=n; i++){
+    total += i;
+    }
+
+  printf("%d\n",total);
+
+  return 0;
+}
