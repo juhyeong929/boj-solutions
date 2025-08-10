a8b57ade6155acb2f4faa164e61e00b9fa97bffa
Jhyeong09 <kimkjo0409@naver.com>
2025-08-10 21:25:33 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int bulgi_year;
+    
+    scanf("%d", &bulgi_year);
+    
+    int seogi_year = bulgi_year - 543;
+    
+    printf("%d\n", seogi_year);
+    
+    return 0;
+}
