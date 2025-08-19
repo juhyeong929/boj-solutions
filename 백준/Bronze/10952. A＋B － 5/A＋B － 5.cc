f5aeb5f343c96aed519f3ae65c468e6291a7467e
Jhyeong09 <kimkjo0409@naver.com>
2025-08-19 18:46:29 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int A, B;
+
+    while (1) {
+        if (scanf("%d %d", &A, &B) != 2) {
+            break;
+        }
+        if (A == 0 && B == 0) {
+            break;
+        }
+        printf("%d\n", A + B);
+    }
+    
+    return 0;
+}
