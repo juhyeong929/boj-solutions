c88f77ffeaf5aff0e79d53b7dc022354f3ff9ed2
Jhyeong09 <kimkjo0409@naver.com>
2025-09-10 16:51:52 +0900

    [Bronze II] Title: 단어의 개수, Time: 8 ms, Memory: 1972 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -1,11 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 1000001
 
 int main(void) {
-    int count = 0;
-    char word[1000001];
+    char str[MAX_SIZE];
+    if (fgets(str, sizeof(str), stdin) == NULL) {
+        printf("0\n");
+        return 0;
+    }
+    str[strcspn(str, "\n")] = 0;
 
-    while (scanf("%s", word) == 1) {
-        count++;
+    int count = 0;
+    int is_word = 0; 
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
+            is_word = 0;
+        } else {
+            // 공백이 아닌 문자가 나타났을 때
+            if (is_word == 0) {
+                is_word = 1;
+                count++;
+            }
+        }
     }
 
     printf("%d\n", count);
